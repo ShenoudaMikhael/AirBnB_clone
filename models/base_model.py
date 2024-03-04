e0e9ab794419bf6ff3b430d79488f77d3021fd1c
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-04 11:33:54 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 """Base model Module"""
+
+
+class Base:
+    """Class Base contains only Id, Creation date, update date."""
+
+    def __init__(self, id, created_at, updated_at):
+        self.id = id
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def save(self):
+        """base function for saving"""
+        return
+
+    def to_json(self):
+        """serilize model to josn object"""
+        return {}
