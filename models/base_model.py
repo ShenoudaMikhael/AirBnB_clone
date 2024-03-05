8a810b09f0177cc0abc666f614d9f65619f8130b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-05 03:32:31 +0200

    storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,28 +2,31 @@
 """Base model Module"""
 import uuid
 from datetime import datetime
+from . import storage
 
 
 class BaseModel:
     """Class Base contains only Id, Creation date, update date."""
 
     def __init__(self, *args, **kwargs):
- 
         if kwargs:
             for k, v in kwargs.items():
                 if k != "__class__":
                     setattr(self, k, v)
-            self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
-            self.updated_at = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.created_at = datetime.strptime(
+                self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(
+                self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
             return
         self.id = uuid.uuid4().__str__()
-        
         self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
         self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        storage.new(self)
 
     def save(self):
         """base function for saving"""
         self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        storage.save()
 
     def to_dict(self):
         """serilize model to dict object"""
@@ -33,6 +36,4 @@ class BaseModel:
 
     def __str__(self) -> str:
         return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id, self.__dict__
-            )
+            self.__class__.__name__, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,22 +1,55 @@
 #!/usr/bin/python3
 """File Storage Module"""
+import json
 
 
 class FileStorage:
     """File Storage Class"""
 
     __file_path = "file.json"
-    __objects = []
+    __objects = {}
 
     def all(self):
         """return all objects"""
-        return self.__objects
+        try:
+            with open(
+                    "{}".format(self.__file_path),
+                    'r', encoding='utf-8') as file:
+                q = file.read()
+                print("AAAAA")
+                self.__objects = json.loads(q)
+            return self.__objects
+        except FileNotFoundError:
+            return self.__objects
 
     def new(self, obj):
-        self.__objects.append({obj.__class__.__name__: {obj}})
+        """add new obj to objects dictionary"""
+        self.__objects[
+            "{}.{}".format(obj.__class__.__name__, obj.id)] = obj.to_dict()
 
     def save(self):
-        pass
+        """Save objects to a file."""
+
+        with open(
+                "{}".format(self.__file_path), "w+", encoding="utf-8") as file:
+            if self.__objects is not None:
+                file.write(json.dumps(self.__objects))
+            else:
+                file.write(json.dumps([]))
 
     def reload(self):
-        pass
+        """deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ;
+        otherwise, do nothing. If the file doesn’t exist,
+        no exception should be raised)
+        """
+        try:
+            with open(
+                    "{}".format(self.__file_path),
+                    "r", encoding="utf-8") as file:
+                items = json.loads(file.read())
+                if not items:
+                    return
+                self.__objects = items
+        except FileNotFoundError:
+            pass
