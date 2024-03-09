ad896a1e7dff0db6b5e09e7ed293547fbfa6e8cd
Salsabil <salsabeelahmed323@gmail.com>
2024-03-09 09:04:44 +0200

    add test_city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,67 +1,56 @@
 #!/usr/bin/python3
 """unittest for user.py"""
 import unittest
-from models.user import User
+from models.city import City
 from datetime import datetime
 
 
-class test_init_user(unittest.TestCase):
-    """Unittests for testing init of User class"""
+class test_init_City(unittest.TestCase):
+    """Unittests for testing init of City class"""
 
     def test_no_args(self):
-        self.assertEqual(User, type(User()))
+        self.assertEqual(City, type(City()))
 
     def test_id(self):
         """id is a string"""
-        self.assertEqual(str, type(User().id))
+        self.assertEqual(str, type(City().id))
 
     def test_created_at(self):
         """created_at is a datetime"""
-        self.assertEqual(datetime, type(User().created_at))
+        self.assertEqual(datetime, type(City().created_at))
 
     def test_updated_at(self):
         """updated_at is a datetime"""
-        self.assertEqual(datetime, type(User().updated_at))
+        self.assertEqual(datetime, type(City().updated_at))
 
-    def test_email(self):
-        """email is a string"""
-        self.assertEqual(str, type(User.email))
+    def test_name(self):
+        """name is a string"""
+        self.assertEqual(str, type(City.name))
 
-    def test_password(self):
-        """password is a string"""
-        self.assertEqual(str, type(User.password))
-
-    def test_first_name(self):
-        """firt_name is a string"""
-        self.assertEqual(str, type(User.first_name))
-
-    def test_last_name(self):
-        """last_name is a string"""
-        self.assertEqual(str, type(User.last_name))
 
     def test_unique_id(self):
         """each id is unique"""
-        user1 = User()
-        user2 = User()
+        user1 = City()
+        user2 = City()
         self.assertNotEqual(user1.id, user2.id)
 
     def test_different_created_at(self):
         """different time when create"""
-        user1 = User()
-        user2 = User()
+        user1 = City()
+        user2 = City()
         self.assertLess(user1.created_at, user2.created_at)
 
     def test_different_updated_at(self):
         """different time when update"""
-        user1 = User()
-        user2 = User()
+        user1 = City()
+        user2 = City()
         self.assertLess(user1.updated_at, user2.updated_at)
     
 class test_User_save(unittest.TestCase):
     """Unittests for save"""
     
     def test_save(self):
-        instance = User()
+        instance = City()
         data = instance.updated_at
         instance.save()
         self.assertNotEqual(data, instance.updated_at)
